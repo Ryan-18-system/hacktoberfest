187c245e947ba27a739a8318d5ede414022263b0
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-10-21 22:36:28 -0300

    adicionando array de seleções

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const times = [{ selecao: "Brasil", pontos: 0, partidas: 0, gols: 0 }, { selecao: "Argentina", pontos: 0, partidas: 0, gols: 0 }, { selecao: "Alemanhã", pontos: 0, partidas: 0, gols: 0 }, { selecao: "Camarões", pontos: 0, partidas: 0, gols: 0 }]
+
+
+console.log(...times)
